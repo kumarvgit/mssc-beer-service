dce00c97389ab0547f73bdf2565e3e3872db5cfa
kumarvgit <vaibhav.cs.kumar@gmail.com>
2019-10-07 11:23:29 +0530

    fix tests. closes #11

--- src/test/java/guru/springframework/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/msscbeerservice/web/controller/BeerControllerTest.java
@@ -2,12 +2,14 @@ package guru.springframework.msscbeerservice.web.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.msscbeerservice.web.model.BeerDto;
+import guru.springframework.msscbeerservice.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -31,8 +33,8 @@ class BeerControllerTest {
 
     @Test
     void saveNewBeer() throws Exception {
-        BeerDto beerDto = BeerDto.builder().build();
-        String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+//        BeerDto beerDto = BeerDto.builder().build(); // this is failing due to constraints added
+        String beerDtoJson = objectMapper.writeValueAsString(getValidDto());
         mockMvc.perform(
                 post("/api/v1/beer/")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -42,11 +44,21 @@ class BeerControllerTest {
 
     @Test
     void updateBeer() throws Exception {
-        BeerDto beerDto = BeerDto.builder().build();
-        String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+//        BeerDto beerDto = BeerDto.builder().build(); // this is failing due to constraints
+        String beerDtoJson = objectMapper.writeValueAsString(getValidDto());
         mockMvc.perform(put("/api/v1/beer/" + UUID.randomUUID().toString())
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(beerDtoJson)
         ).andExpect(status().isNoContent());
+    }
+
+    BeerDto getValidDto() {
+        return BeerDto
+                .builder()
+                .beerName("New beer")
+                .beerStyle(BeerStyleEnum.ALE)
+                .price(new BigDecimal("1.99"))
+                .upc(12345678913L)
+                .build();
     }
 }
