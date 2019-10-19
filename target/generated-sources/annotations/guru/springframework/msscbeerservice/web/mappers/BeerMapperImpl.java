63dcecd73f13696d8ced53685deeab3cf72ef0f4
kumarvgit <vaibhav.cs.kumar@gmail.com>
2019-10-20 01:52:41 +0530

    using JT bom

--- target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/msscbeerservice/web/mappers/BeerMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2019-10-14T05:01:14+0530",
+    date = "2019-10-20T01:44:28+0530",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.4 (Ubuntu)"
 )
 @Component
